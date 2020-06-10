658b1a6bb2f24abb47db523f389d3ac648d647df
Paulina Arnesson <paulinaarnesson@hotmail.com>
2020-06-10 15:38:06 +0200

    Click event working for one player, and we can now compare startTime and clickTime

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -2,10 +2,34 @@
  * Socket controller
  */
 
-const debug = require('debug')('09-simple-chat:socket_controller');
+const debug = require('debug')('virus_game:socket_controller');
 
 const players = {};
+let startTime = null;
 
+/**
+ * handle and compare click time to start time
+ */
+function handleCompareClick() {
+	const clickTime = Date.now();
+	const differenceTime = startTime - clickTime;
+	console.log(`${players[this.id]} managed to click the virus in ${differenceTime} time`);
+}
+
+/**
+ * Handle user disconnecting
+ */
+function handleDisconnect() {
+	debug(`Socket ${this.id} left the chat :(`);
+
+	// broadcast to all connected sockets that this player has left the chat
+	if (players[this.id]) {
+		this.broadcast.emit('player-disconnected', players[this.id]);
+	}
+
+	// remove player from list of connected players
+	delete players[this.id];
+}
 /**
  * Get nicknames of online players
  */
@@ -17,11 +41,10 @@ function getOnlinePlayers() {
  * Handle a new player connecting
  */
 function handleRegisterPlayer(player_name, callback) {
-	debug("Player '%s' connected to the game", player_name);
+	debug("Player connected to the game", player_name);
 	players[this.id] = player_name;
 	callback({
 		joinGame: true,
-		//usernameInUse: false,
 		onlinePlayers: getOnlinePlayers(),
 	});
 
@@ -32,12 +55,22 @@ function handleRegisterPlayer(player_name, callback) {
 	 this.broadcast.emit('online-players', getOnlinePlayers());
 }
 
+/**
+ * Handle and start the game
+ */
+function handleStartGame(){
+	startTime = Date.now();
+	this.emit('game-started');
+}
+
 module.exports = function(socket) {
 	debug(`Client ${socket.id} connected!`);
 
-	socket.on('disconnect', () => {
-		debug("Someone left the chat :(");
-	});
+	socket.on('compare-click', handleCompareClick);
+
+	socket.on('disconnect', handleDisconnect);
+
+	socket.on('start-game', handleStartGame);
 
 	socket.on('register-player', handleRegisterPlayer);
 }
--- public/assets/js/virus_game.js
+++ public/assets/js/virus_game.js
@@ -2,14 +2,19 @@ const socket = io();
 
 const player_name_form = document.querySelector('#player_name-form');
 const sign_in = document.querySelector('#sign_in');
+const virus = document.querySelector('#virus');
 const wrapper = document.querySelector('#wrapper');
 
 const addPlayerToList = (player) => {
-	document.querySelector('#players').innerHTML += `<li id="player">${player}</li>`;
+	document.querySelector('#players').innerHTML += `<li id="player">${player}<span>Your time</span></li>`;
 }
 
 const updateOnlinePlayers = (players) => {
-	document.querySelector('#players').innerHTML = players.map(player => `<li class="player">${player}</li>`).join("");
+	document.querySelector('#players').innerHTML = players.map(player => `<li class="player">${player}<span>Your time</span></li>`).join("");
+
+	if(players.length === 2){
+		socket.emit('start-game', (players));
+	}
 }
 
 
@@ -28,12 +33,27 @@ player_name_form.addEventListener('submit', e => {
 	});
 });
 
-socket.on('player-connected', player_name => {
-	addPlayerToList(player_name);
+socket.on('game-started', () => {
+	virus.addEventListener('click', () => {
+		socket.emit('compare-click');
+	});
 });
 
 socket.on('online-players', (players) => {
 	updateOnlinePlayers(players);
 });
+
+socket.on('player-disconnected', (player) => {
+	alert(`Your opponent ${player} gave up and you won the game!`);
+
+	sign_in.classList.remove('hide');
+	wrapper.classList.add('hide');
+});
+
+socket.on('player-connected', player_name => {
+	addPlayerToList(player_name);
+});
+
+
 
 
